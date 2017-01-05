d15006f63a7a15f35afa15db0e601a6e4c82a745
frankie <franklin.shaw@gmail.com>
2017-01-05 16:00:08 +0000

    step11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,31 @@
 class DockingStation
-
+  attr_reader :bike
   def release_bike
     return Bike.new
   end
 
-def dock(bike)
+  def dock(bike)
+    @bike = bike
+  end
+
 
 end
 
-end
+
+
+=begin
+nouns           verbs
+mop             decide
+docking station see
+bike            docked
+
+objects         messages
+mop             decide
+docking station see, decide, use?
+bike            see?
+
+write a feature test for seeing a bike has been docked
+write a unit test for the method you need to add to DockingStation/Bike to make seeing posible
+pass both tests
+use an instance variable with attr_reader to do a full test-implementation cycle for the second User Story above
+=end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,9 +13,24 @@ describe DockingStation do
     expect(((DockingStation.new).release_bike).working?).to eq true
   end
 
-  it 'docks a bike' do
+  it 'subject responding with 1 argument' do
     expect(subject).to respond_to(:dock).with(1).argument
   end
 
+  it 'subject responds to .bike' do
+    expect(subject).to respond_to :bike
+  end
+
+#walkthrough
+  it 'docks the bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
 end
